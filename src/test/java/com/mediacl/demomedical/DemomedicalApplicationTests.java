45a5549487a833a95a43d2252b2a160e84123887
RedwoodTechno <consultantdineshkumar@gmail.com>
2026-02-02 20:41:06 +0530

    Initial commit

---
+++ src/test/java/com/mediacl/demomedical/DemomedicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediacl.demomedical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomedicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
